a8dde49ed9b1f9317de0aed7702738de4450474e
王浩然 <309568486@qq.com>
2017-01-23 17:51:37 +0800

    修改了脚本

--- githubAction.php
+++ githubAction.php
@@ -25,13 +25,13 @@ class temp extends githubAction{
         $this->checkout($this->runLocalBranch);
     }
     public function main(){
-        $newBranchName = '创建临时分支';
-        $this->createBranch($newBranchName);
-        mkdir('include');
-        $parentClass = classAction::createClass('tempParentClass','','','autoLoadClass');
-        $parentClass->save();
-        $this->checkout($this->runLocalBranch);
-        $this->mergeBranch($newBranchName);
+//        $newBranchName = '创建临时分支';
+//        $this->createBranch($newBranchName);
+//        mkdir('include');
+//        $parentClass = classAction::createClass('tempParentClass','','','autoLoadClass');
+//        $parentClass->save();
+//        $this->checkout($this->runLocalBranch);
+//        $this->mergeBranch($newBranchName);
     }
 }
 $a = new temp();
