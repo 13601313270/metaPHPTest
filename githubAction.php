55f3270c44add84b5b8fc5bf0b960a4d237f29db
王浩然 <309568486@qq.com>
2017-01-23 15:40:24 +0800

    初始化，增加了接收github函数

---
+++ githubAction.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfw
+ * Date: 2017/1/23
+ * Time: 下午2:56
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
+include_once('./metaPHP/githubAction.php');
+class temp extends githubAction{
+    protected $listenBranch = array(
+        'refs/heads/master',
+        'refs/heads/develop'
+    );
+}
+$a = new temp();
+$a->run();
---
+++ include.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfw
+ * Date: 2017/1/22
+ * Time: 上午1:39
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set('PRC');
+function autoLoadClass($className){
+    return 'include/'.$className.'.php';
+}
+//闭合
+spl_autoload_register(function($class){
+    $classPath = autoLoadClass($class);
+    include_once($classPath);
+});
