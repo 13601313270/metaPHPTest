c6fc211726f747f922b6c76b56bb6f9066d413c3
王浩然 <309568486@qq.com>
2017-01-24 14:38:40 +0800

    接收程序demo完成

--- githubAction.php
+++ githubAction.php
@@ -11,20 +11,17 @@ include_once('./include.php');
 include_once('./metaPHP/githubAction.php');
 class temp extends githubAction{
     public $runLocalBranch = 'develop';//正在运行的本地分支
-    public $runBranch = 'refs/heads/develop';//正在运行的本地分支对应的远程分支
     public $webRootDir = '/var/www/html/metaPHPTest';
     public $cachePath = '/var/www/html/metaPHPTest/metaPHPCacheFile';
-    protected $listenBranch = array(
-        'refs/heads/master',
-        'refs/heads/develop'
-    );
     public function run()
     {
-        
-//        if(empty($this->listenBranch)){exit;}
-//        if(empty($this->webRootDir)){exit;}
-//        $response = json_decode(file_get_contents('php://input'));
-//        if(!in_array($response->ref,$this->listenBranch)){exit;}
+        $response = json_decode(file_get_contents('php://input'));
+        if(!in_array($response->ref,array(
+            'refs/heads/master',
+            'refs/heads/develop'
+        ))){
+            exit;
+        }
         parent::pull();
         $this->main();
         $this->checkout($this->runLocalBranch);
