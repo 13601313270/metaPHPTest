ae5dd0c5976f01a8e74f92372512d5c916b3e2a1
王浩然 <309568486@qq.com>
2017-01-25 04:56:40 +0800

    放到暂存区

--- githubAction.php
+++ githubAction.php
@@ -26,7 +26,8 @@ class temp extends githubAction{
 //        var_dump("==========合并==========\n");
         $this->mergeBranch($newBranchName);
 //        var_dump("==========commit==========\n");
-        $this->commit('合并分支:'.$newBranchName.'到'.$this->runLocalBranch);
+        $result = $this->exec('cd ' . $this->webRootDir . ';git add --all;');
+//        $this->commit('合并分支:'.$newBranchName.'到'.$this->runLocalBranch);
 //        $this->deleteBranch($newBranchName);
 //        var_dump("==========push==========\n");
         $this->push();
