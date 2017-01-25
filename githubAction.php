b9d9c3df2cc5796a8ad476404263d02b2e3a36e8
王浩然 <309568486@qq.com>
2017-01-25 09:08:18 +0800

    更改了输出

--- githubAction.php
+++ githubAction.php
@@ -18,17 +18,18 @@ class temp extends githubAction{
     public function main(){
         $newBranchName = '增加了内容';
         $this->branchClean();
+        var_dump("\n==========创建分支==========\n");
         $this->createBranch($newBranchName);
         file_put_contents('ceshi.txt','4'."\n",FILE_APPEND);
         $this->commit('删除了ceshi.txt');
-        var_dump("==========提交==========\n");
+        var_dump("\n==========提交==========\n");
         $this->checkout($this->runLocalBranch);
-        var_dump("==========合并==========\n");
+        var_dump("\n==========合并==========\n");
         $this->mergeBranch($newBranchName);
-        var_dump("==========commit==========\n");
+        var_dump("\n==========commit==========\n");
         $this->commit('合并分支:'.$newBranchName.'到'.$this->runLocalBranch);
         $this->deleteBranch($newBranchName);
-        var_dump("==========push==========\n");
+        var_dump("\n==========push==========\n");
         $this->push();
     }
     public function run()
