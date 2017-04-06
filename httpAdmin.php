25f14e14f2e555ac528962fc2dde2726d8157d13
metaPHPRobot <wanghaoran5555@sohu.com>
2017-04-07 01:53:43 +0800

    修改httpAdmin.php文件配置kod_web_page实例的httpFileConfig属性index.php改为首页123

--- httpAdmin.php
+++ httpAdmin.php
@@ -1,15 +1,15 @@
 <?php
-;
+
 /**
  * Created by PhpStorm.
  * User: 王浩然
  * Date: 2017/3/20
  * Time: 下午2:14
  */
-;
+
 include_once('include.php');
 $page=new kod_web_page();
 $page2=new stdClass();
 $page->fileList=scandir('./http/');
-$page->httpFileConfig=array('index.php' => '首页111');
+$page->httpFileConfig=array('index.php' => '首页123');
 $page->fetch('httpAdmin.tpl');
