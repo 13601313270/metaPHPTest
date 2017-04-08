63135c03bf098974535a4df188e6807a935995db
metaPHPRobot <wanghaoran5555@sohu.com>
2017-04-08 18:36:03 +0800

    修改httpAdmin.php文件配置kod_web_page实例的httpFileConfig属性index.php改为新名字

--- httpAdmin.php
+++ httpAdmin.php
@@ -11,5 +11,5 @@ include_once('include.php');
 $page=new kod_web_page();
 $page2=new stdClass();
 $page->fileList=scandir('./http/');
-$page->httpFileConfig=array('index.php' => 'new');
+$page->httpFileConfig=array('index.php' => '新名字');
 $page->fetch('httpAdmin.tpl');
