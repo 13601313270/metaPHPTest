29847644746b7ccda7b418b22082f2d5c4272adf
metaPHPRobot <wanghaoran5555@sohu.com>
2017-04-23 17:09:23 +0800

    增加了表article的操作接口类

--- include/article.php
+++ include/article.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * 表article操作接口
- *
- * User: metaPHP
- * Date: 2017/04/23
- * Time: 15:26
- */
+* 表article操作接口
+*
+* User: metaPHP
+* Date: 2017/04/23
+* Time: 17:09
+*/
 
 class article extends kod_db_mysqlSingle{
     protected $tableName = 'article';
