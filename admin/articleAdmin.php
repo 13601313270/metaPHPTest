bf14bf7ae916f25cf1a7f8bd9a4e37f52a2eff62
王浩然 <309568486@qq.com>
2017-04-27 13:31:39 +0800

    去掉了int类型的错误default

--- admin/articleAdmin.php
+++ admin/articleAdmin.php
@@ -21,7 +21,6 @@ class articleAdmin extends kod_web_mysqlAdmin{
             'notNull' => "true",
             'title' => 'id',
             'AUTO_INCREMENT' => true,
-            'default' => ''
         ),
         'title' => array(
             'dataType' => 'varchar',
@@ -54,28 +53,24 @@ class articleAdmin extends kod_web_mysqlAdmin{
             'maxLength' => 11,
             'notNull' => "true",
             'title' => '分类',
-            'default' => ''
         ),
         'project' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => "true",
             'title' => '项目',
-            'default' => ''
         ),
         'good' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => "true",
             'title' => '点赞',
-            'default' => '0'
         ),
         'bad' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => "true",
             'title' => '差评',
-            'default' => '0'
         )
     );
     public function main(){
