9bea275eebd96d756c3f28ffec72d1c1e776355e
王浩然 <309568486@qq.com>
2017-04-27 13:37:28 +0800

    修正

--- admin/articleAdmin.php
+++ admin/articleAdmin.php
@@ -21,61 +21,52 @@ class articleAdmin extends kod_web_mysqlAdmin{
             'notNull' => true,
             'title' => 'id',
             'AUTO_INCREMENT' => true,
-            'default' => 
         ),
         'title' => array(
             'dataType' => 'varchar',
             'maxLength' => 255,
             'notNull' => true,
             'title' => '标题',
-            'default' => 
         ),
         'img' => array(
             'dataType' => 'imageQiniu',
             'maxLength' => 255,
             'notNull' => true,
             'title' => '列表图',
-            'default' => 
         ),
         'ctime' => array(
             'dataType' => 'datetime',
             'notNull' => true,
             'title' => '创建时间',
-            'default' => 
         ),
         'content' => array(
             'dataType' => 'text',
             'notNull' => true,
             'title' => '正文',
-            'default' => 
         ),
         'type' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => true,
             'title' => '分类',
-            'default' => 
         ),
         'project' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => true,
             'title' => '项目',
-            'default' => 
         ),
         'good' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => true,
             'title' => '点赞',
-            'default' => 
         ),
         'bad' => array(
             'dataType' => 'int',
             'maxLength' => 11,
             'notNull' => true,
             'title' => '差评',
-            'default' => 
         )
     );
     public function main(){
