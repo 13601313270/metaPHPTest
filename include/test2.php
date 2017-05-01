44c61d79ed38f154858678bc755bcdb6e4aa1b10
metaPHPRobot <wanghaoran5555@sohu.com>
2017-05-01 21:35:33 +0800

    增加了表test2的操作接口类

---
+++ include/test2.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* 表test2操作接口
+*
+* User: metaPHP
+* Date: 2017/05/01
+* Time: 21:35
+*/
+
+class test2 extends kod_db_mysqlSingle{
+    protected $tableName = 'test2';
+    protected $key = 'name';
+    protected $keyDataType = 'varchar';
+}
+
