e2191eae1b94a68fa025c36127c7a1e078e97661
王浩然 <309568486@qq.com>
2017-05-03 07:34:42 +0800

    删除了不存在的字段

--- admin/test2Admin.php
+++ admin/test2Admin.php
@@ -35,12 +35,6 @@ class test2Admin extends kod_web_mysqlAdmin{
             'notNull' => true,
             'title' => 'title',
             'unique' => true
-        ),
-        'ddd' => array(
-            'dataType' => 'int',
-            'title' => '巴拉',
-            'primarykey' => true,
-            'name' => 'ddd'
         )
     );
     public function main(){
